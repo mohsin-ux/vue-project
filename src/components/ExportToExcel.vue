f7639ae7fa3372fec1bc93ff9136f5394260c399
Mohsin Raazi <mohsinrazakhan4455@gmail.com>
2025-01-08 18:08:41 +0500

    feat: gives the width of column according to the content

--- src/components/ExportToExcel.vue
+++ src/components/ExportToExcel.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {  utils, writeFile } from "xlsx";
+import { utils, writeFile } from "xlsx";
 
 const { data } = defineProps<{
   data: any[];
@@ -9,6 +9,26 @@ function exportToExcel() {
   try {
     // Create a worksheet
     const worksheet = utils.json_to_sheet(data);
+
+    // Set column widths
+    worksheet["!cols"] = [
+      { wch: 20 },
+      { wch: 35 },
+      { wch: 15 },
+      { wch: 15 },
+      { wch: 15 },
+    ];
+
+    // Bold the header row (first row)
+    const headers = Object.keys(data[0] || {});
+    headers.forEach((_, index) => {
+      const cellAddress = utils.encode_cell({ r: 0, c: index }); // Row 0, each column
+      if (worksheet[cellAddress]) {
+        worksheet[cellAddress].s = {
+          font: { bold: true }, // Apply bold style
+        };
+      }
+    });
 
     // Create a workbook
     const workbook = utils.book_new();
@@ -24,7 +44,11 @@ function exportToExcel() {
 }
 </script>
 <template>
-  <button @click="exportToExcel" class="flex flex-col justify-center items-center w-full pt-10">
-    <img class="w-32 h-" src="/plane-icon.svg" alt="hello">
-    Export JSON to Excel</button>
+  <button
+    @click="exportToExcel"
+    class="flex flex-col justify-center items-center w-full pt-10"
+  >
+    <img class="w-32 h-" src="/plane-icon.svg" alt="hello" />
+    Export JSON to Excel
+  </button>
 </template>
